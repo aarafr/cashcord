a491fb017c7a389da0f576b2f99e9803f5233c86
Lukas <vanderspiegel.lukas@gmail.com>
2023-03-13 16:59:33 +0100

    standard file infrastructure fixed (gitignore, server (index.ts), .json, etc.

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-// server code
+import path from 'path';
+const express = require('express');
+const ejs = require('EJS');
+
+const app = express();
+app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req:any, res:any) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(app.get('port'), 
+    ()=>console.log( '[server] http://localhost:' + app.get('port')));
