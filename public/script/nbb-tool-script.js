8c63e68a55f9bdf1065790df975ec05a977cab80
Kobe Bervoets <kobe.bervoets@ap.be>
2023-03-20 17:01:51 +0100

    toevoegen nbb tool html, css en js

---
+++ public/script/nbb-tool-script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector(".form");
+const results = document.querySelector(".results");
+
+form.addEventListener("submit", (event) => {
+  results.style.display = "flex";
+});
