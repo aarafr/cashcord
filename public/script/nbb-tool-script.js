f66094f02e1519a1dcb9732e47c7e39d9eafbe94
Kobe Bervoets <kobe.bervoets@ap.be>
2023-03-27 11:34:21 +0200

    geschiedenins sectie toegevoegd aan tool pagina

--- public/script/nbb-tool-script.js
+++ public/script/nbb-tool-script.js
@@ -1,6 +1,20 @@
 const form = document.querySelector(".form");
 const results = document.querySelector(".results");
+const historyList = document.querySelectorAll(".list-item");
 
 form.addEventListener("submit", (event) => {
+  event.preventDefault();
   results.style.display = "flex";
 });
+
+for (let i = 0; i < historyList.length; i++) {
+  historyList[i].addEventListener("click", function () {
+    historyList[i].classList.toggle("active");
+    const content = historyList[i].querySelector(".hidden-content");
+    if (content.style.display === "flex") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "flex";
+    }
+  });
+}
