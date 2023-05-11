f7ea4780d8ad328dc5ded9f3f09b121a4b202db2
Kobe Bervoets <kobe.bervoets@ap.be>
2023-05-11 17:59:10 +0200

    aanpassing signup pagina. Toevoeging error message wanneer registratie criteria niet voldoed. Aanpassing regex

--- index.ts
+++ index.ts
@@ -1,28 +1,24 @@
 import path from "path";
-import express from 'express';
+import express from "express";
 import { MongoClient } from "mongodb";
-const bodyParser = require('body-parser'); // npm install body-parser
+const bodyParser = require("body-parser"); // npm install body-parser
 // const { body, validationResult } = require('express-validator'); // npm install express-validator
 
-
 interface User {
-  name: string,
-  email: string,
-  password: string
+  name: string;
+  email: string;
+  password: string;
 }
 
-let userArray : User[] = [];
-
+let userArray: User[] = [];
 
 let MongoPassword = encodeURIComponent("hhfrp132545ppokhh1");
-const url = `mongodb+srv://Sidge:${MongoPassword}@cashcord.m5mpiy9.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://nodejs_user:NAgARzje8W6aosBL@cluster0.tzj4rtf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(url);
-
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 // let api = "https://ws.uat2.cbso.nbb.be/authentic/legalEntity/0453488757";
 // fetch(api)
@@ -33,104 +29,110 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //     return response.json;
 //   })
 
-
 app.set("port", 3000);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
+app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.get("/", (req, res) => {
-  res.type('text/html'); 
-  res.render('index', {path: req.path});
+  res.type("text/html");
+  res.render("index", { path: req.path });
 });
 
 app.get("/login", (req, res) => {
-  res.type('text/html'); 
-  res.render('login', {path: req.path});
-})
+  res.type("text/html");
+  res.render("login", { path: req.path });
+});
 
-
-app.post('/signup', (req, res) => {
+app.post("/signup", (req, res) => {
   console.log(req.body);
   const name = req.body.firstname + " " + req.body.lastname;
-  const email = req.body.email.replace(/\s+/g, '');
+  const email = req.body.email.replace(/\s+/g, "");
   const password = req.body.password;
   const passwordRepeat = req.body.passwordRepeat;
 
-  const nameMatch = /^[a-zA-Z]+$/;
-  const mailMatch = /^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/;
+  const nameMatch = /^[a-zA-Z ]+$/;
+  const mailMatch = /^[a-zA-Z0-9.]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/;
   const passMatch = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
 
-  if(!name.match(nameMatch)){
-    res.status(400);
-    return;
+  if (!name.match(nameMatch)) {
+    return res.render("signup", {
+      path: req.path,
+      loginError: {
+        message: "De ingevoerde naam is ongeldig",
+      },
+    });
   }
-  if(!email.match(mailMatch)){
-    res.status(400);
-    return;
+  if (!email.match(mailMatch)) {
+    return res.render("signup", {
+      path: req.path,
+      loginError: {
+        message: "Het ingevoerde email adres is ongeldig",
+      },
+    });
   }
-  if(password !== passwordRepeat){
-    res.status(400);
-    return;
+  if (password !== passwordRepeat) {
+    return res.render("signup", {
+      path: req.path,
+      loginError: {
+        message: "De ingevoerde wachtwoorden zijn niet gelijk",
+      },
+    });
   }
-  if(!password.match(passMatch)){
-    res.status(400);
-    return;
+  if (!password.match(passMatch)) {
+    return res.render("signup", {
+      path: req.path,
+      loginError: {
+        message:
+          "Je wachtwoord moet minimaal 8 tekens bevatten, waarvan minstens 1 cijfer, 1 kleine letter, 1 hoofdletter en minstens 1 van deze karakters !@#$%^&*",
+      },
+    });
   }
   res.status(200);
-  res.redirect('/');
-  const userObj : User = {
+  res.redirect("/");
+  const userObj: User = {
     name: name,
     email: email,
-    password: password
-  }
+    password: password,
+  };
   console.log(userObj);
   data(userObj);
 });
 
 app.get("/signup", (req, res) => {
-  res.type('text/html'); 
-  res.render('signup', {path: req.path});
-})
-
+  res.type("text/html");
+  res.render("signup", { path: req.path });
+});
 
 app.get("/compare", (req, res) => {
-  res.type('text/html'); 
-  res.render('compare', {path: req.path});
+  res.type("text/html");
+  res.render("compare", { path: req.path });
 });
 
+const data = async (userObj: User) => {
+  try {
+    await client.connect();
+    console.log("connected to the database");
 
-const data = async(userObj : User) => {
-  try{
-      await client.connect();
-      console.log("connected to the database");
-      
-      userArray.push(userObj);
+    userArray.push(userObj);
 
-      let users : User[] = await client.db("cashcord").collection("users").find<User>({}).toArray();
-      if(users.length == 0){
-        await client.db("cashcord").collection("users").insertMany(userArray);
-      }
+    let users: User[] = await client.db("cashcord").collection("users").find<User>({}).toArray();
+    if (users.length == 0) {
+      await client.db("cashcord").collection("users").insertMany(userArray);
+    }
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
   }
-  catch (e) {
-      console.error(e);
-  }
-  finally{
-      await client.close();
-  }
-}
+};
 
-
-
-app.use(function(req, res, next) {
-  res.status(404).render('404');
+app.use(function (req, res, next) {
+  res.status(404).render("404");
 });
 
-app.listen(app.get("port"), () =>
-  console.log("[server] http://localhost:" + app.get("port"))
-);
+app.listen(app.get("port"), () => console.log("[server] http://localhost:" + app.get("port")));
